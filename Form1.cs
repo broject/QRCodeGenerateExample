37d209d9d3a49cd8d752b5bfe09b6949cb6f2709
broject <boroo_c@yahoo.com>
2016-04-10 16:24:40 +0900

    first commit

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QRCodeGenerateExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var url = string.Format("http://chart.apis.google.com/chart?cht=qr&chs={1}x{2}&chl={0}", textBox1.Text, 200, 200);
+            WebResponse response = default(WebResponse);
+            Stream remoteStream = default(Stream);
+            StreamReader readStream = default(StreamReader);
+            WebRequest request = WebRequest.Create(url);
+            response = request.GetResponse();
+            remoteStream = response.GetResponseStream();
+            readStream = new StreamReader(remoteStream);
+            System.Drawing.Image img = System.Drawing.Image.FromStream(remoteStream);
+            img.Save("D:/" + textBox1.Text + ".png");
+            response.Close();
+            remoteStream.Close();
+            readStream.Close();
+
+            pictureBox1.Image = new Bitmap("D:\\" + textBox1.Text + ".png");
+        }
+    }
+}
